f5d286afb03359363d712556ac561f22da5a63f8
EnesAlpay <152078121+EnesAlpay@users.noreply.github.com>
2024-11-14 15:01:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ from scapy.all import sniff
 import threading
 
 def packet_callback(packet):
-    # Paket bilgilerini metin kutusuna ekle
     output_text.insert(tk.END, f"{packet.summary()}\n")
-    output_text.yview(tk.END)  # En son eklenen metne kaydır
+    output_text.yview(tk.END)  
 
 def start_capture():
-    # Ağ trafiğini yakalamak için scapy kullan
-    sniff(prn=packet_callback, iface="eth0")  # 'eth0' arayüzünü kullan
+
+    sniff(prn=packet_callback, iface="eth0")
 
 def start_sniffing_thread():
-    # Yeni bir iş parçacığı başlat
+
     threading.Thread(target=start_capture, daemon=True).start()
 
-# GUI oluşturma
+
 root = tk.Tk()
 root.title("Ağ Trafiği Yakalama Aracı")
 
-# Metin kutusu oluştur
+
 output_text = scrolledtext.ScrolledText(root, width=80, height=20)
 output_text.pack()
 
-# Başlat butonu
+
 start_button = tk.Button(root, text="Başlat", command=start_sniffing_thread)
 start_button.pack()
 
